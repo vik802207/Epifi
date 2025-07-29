2b321d23c52479d93bc80df806ed2238bab76ffd
Vikash Gupta <137061206+vik802207@users.noreply.github.com>
2025-07-29 18:51:47 +0530

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:5000/api",
+        url: "https://epifi.onrender.com/api",
         description: "Development server"
       }
     ],
